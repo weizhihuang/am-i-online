2082aab8cf21891f20233303914f1bc0e6d44f05
黃威智 <huangweizhi@huangweizhideMacBook-Pro.local>
2019-11-29 03:40:19 +0800

    ping main function

--- src/views/Ping.vue
+++ src/views/Ping.vue
@@ -1,14 +1,29 @@
 <template lang="pug">
-  span(ref='ping') 5000
+  span(class='d-block text-right white--text' ref='ping') {{ ping }}
 </template>
 
 <script>
 import { ipcRenderer } from 'electron'
+import { ping } from 'tcp-ping'
 
 export default {
   name: 'ping',
+  data: () => ({
+    ping: 'initializing',
+    intervalId: null
+  }),
   mounted() {
     this.handleWindow()
+    this.intervalId = setInterval(
+      () =>
+        ping({ address: '8.8.8.8', port: 53, attempts: 1 }, (err, { avg }) => {
+          this.ping = isNaN(avg) ? 'timeout' : (~~avg).toString()
+        }),
+      1000
+    )
+  },
+  beforeDestroy() {
+    clearInterval(this.intervalId)
   },
   methods: {
     handleWindow() {
