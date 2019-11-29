16e2b4f9815defef946ad1745e6114fb4a786121
黃威智 <huangweizhi@huangweizhideMacBook-Pro.local>
2019-11-30 04:59:00 +0800

    fix

--- publish_for_windows_with_docker.sh
+++ publish_for_windows_with_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker run --rm -ti \
- --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
- --env ELECTRON_CACHE="/root/.cache/electron" \
- --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
- --env GH_TOKEN="${GH_TOKEN}" \
+ -e-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
+ -e ELECTRON_CACHE="/root/.cache/electron" \
+ -e ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
+ -e GH_TOKEN="${GH_TOKEN}" \
  -v ${PWD}:/project \
  -v ${PWD##*/}-node-modules:/project/node_modules \
  -v ~/.cache/electron:/root/.cache/electron \
