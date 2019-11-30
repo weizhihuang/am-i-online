820e2700a44218e1aa7023e7b0ba91c4191a7bd7
黃威智 <huangweizhi@huangweizhideMacBook-Pro.local>
2019-11-30 21:48:06 +0800

    fix script

--- build_for_windows_with_docker.sh
+++ build_for_windows_with_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --rm -ti \
- -e-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
+ --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
  -e ELECTRON_CACHE="/root/.cache/electron" \
  -e ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
  -e GH_TOKEN="${GH_TOKEN}" \
@@ -8,4 +8,4 @@ docker run --rm -ti \
  -v ${PWD##*/}-node-modules:/project/node_modules \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \
- electronuserland/builder:wine /bin/bash -c "yarn && yarn electron:build -w -p always"
+ electronuserland/builder:wine /bin/bash -c "yarn && yarn electron:build -w $*"
